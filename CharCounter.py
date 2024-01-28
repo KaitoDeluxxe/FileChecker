5794e0fd9c237998b8ee64d2d1c3b58690a3f565
Kaito Deluxe <150624897+KaitoDeluxxe@users.noreply.github.com>
2024-01-28 13:35:39 +0700

    Update CharCounter.py

--- CharCounter.py
+++ CharCounter.py
@@ -3,7 +3,7 @@ from colorama import Fore,Style
 def main():
     print(f"{Fore.LIGHTWHITE_EX}String and Hashes Calculator{Style.RESET_ALL}")
     x = input(f"{Fore.LIGHTWHITE_EX}Enter String : {Style.RESET_ALL}")
-    print(f"{Fore.LIGHTGREEN_EX}{len(x.strip(" "))}{Style.RESET_ALL}")
+    print(f"{Fore.LIGHTGREEN_EX}{len(x.strip())}{Style.RESET_ALL}")
     hashes = {"MD5":32,"SHA1":40,"SHA224":56,"SHA256":64,"SHA384":96,"SHA512":128}
     for hash,digit in hashes.items():
         print((f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} {Fore.CYAN}{hash}{Style.RESET_ALL} hash is {Fore.GREEN}{digit}{Style.RESET_ALL} digits"))
